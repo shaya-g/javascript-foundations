13f519f902da68ff2134ef4cd6408975d475fc44
Shaya Golafshani <s.golaff@gmail.com>
2022-07-12 18:45:06 -0600

    Update the dragon test and file

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,30 @@
+class Dragon {
+  constructor(name, riderName) {
+    this.name = name;
+    this.rider = riderName;
+    this.hungry = true;
+    this.eaten = 0;
+  }
 
+  greet() {
+     return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.eaten++;
+    if (this.eaten >= 3) {
+      this.hungry = false;
+    }
+  }
+
+// isWhite() {
+//   return false;
+// }
+//
+// says(phrase) {
+//   return `**;* ${phrase} *;**`
+// }
+
+}
+
+module.exports = Dragon;
